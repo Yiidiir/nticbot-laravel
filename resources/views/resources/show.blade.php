2ce88bddabea8fb9a2f55857bddf91ffc5768a84
Yidir <yidirbouhadjer@gmail.com>
2018-02-13 03:05:33 +0100

    Added show view

--- resources/views/resources/show.blade.php
+++ resources/views/resources/show.blade.php
@@ -1,7 +1,58 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Yidir
- * Date: 2/11/2018
- * Time: 2:46 AM
- */
+@extends('layouts.app')
+
+
+@section('content')
+
+    <div class="container col-md-6 col-md-offset-3">
+
+        <div class="row">
+
+            <div class="pull-left">
+
+                <h2> Show Resource</h2>
+
+            </div>
+
+            <div class="pull-right">
+
+                <a class="btn btn-primary" href="{{ route('resources.index') }}"> Back</a>
+
+            </div>
+
+        </div>
+
+
+        <div class="row">
+
+
+            <div class="form-group">
+
+                <strong>Title:</strong>
+
+                {{ $resource->title}}
+
+            </div>
+
+
+            <div class="form-group">
+
+                <strong>Body:</strong>
+
+                {{ $resource->description}}
+
+            </div>
+
+            <div class="form-group">
+
+                <strong>Year:</strong>
+
+                {{ $resource->publish_year}}
+
+            </div>
+
+
+        </div>
+
+    </div>
+
+@endsection
