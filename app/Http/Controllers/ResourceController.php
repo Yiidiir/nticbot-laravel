aa3d5814e38d250351b01d0373b8e580605c8388
Yidir <yidirbouhadjer@gmail.com>
2018-02-18 02:23:31 +0100

    Fixed Resource Model Create & Update, fixed validate()

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -19,6 +19,18 @@ class ResourceController extends Controller
     protected $fillable = ['title', 'description', 'google_drive', 'publish_year', 'module_id'];
 
 
+    protected $casts = ['module_id' => 'int'];
+
+    /**
+     * ResourceController constructor.
+     * @param array $fillable
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
     /**
      * Display a listing of the resource.
      *
@@ -27,6 +39,7 @@ class ResourceController extends Controller
     public function index()
     {
         //
+
         $resources = MyResource::latest()->paginate(5);
 
         return view('resources.index', compact('resources'))
@@ -41,22 +54,8 @@ class ResourceController extends Controller
     public function create()
     {
         //
-        $modules = Module::get(['name', 'degree', 'semester', 'code'])->sortBy('semester')->groupBy('degree')->toArray();
 
-        $m_form = array();
-        foreach ($modules as $degree_key => $degree) {
-
-            foreach ($degree as $k_module => $module) {
-                $m_form[$degree_key][$k_module] = 'S' . $module['semester'] . ' > ' . $module['name'];
-            }
-        }
-
-        array_key_exists('L', $m_form) ? ($m_form['Licence'] = $m_form['L'] AND $m_form = array_diff_key($m_form, array_flip((array)['L']))) : NULL;
-        array_key_exists('M', $m_form) ? ($m_form['Master'] = $m_form['M'] AND $m_form = array_diff_key($m_form, array_flip((array)['M']))) : NULL;
-        array_key_exists('D', $m_form) ? ($m_form['Doctorat'] = $m_form['D'] AND $m_form = array_diff_key($m_form, array_flip((array)['D']))) : NULL;
-
-        $modules = $m_form;
-
+        $modules = Module::getFormModulesArray();
 
         return view('resources.create', compact('modules'));
     }
@@ -71,8 +70,10 @@ class ResourceController extends Controller
     {
         //
         /*exit(var_dump($request->all()));*/
-//        exit(var_dump($request->get('publish_year')));
-        $request->validate(['title' => 'required|string', 'description' => 'nullable|string', 'google_drive' => 'nullable|url|max:255', 'publish_year' => 'required|numeric|digits:4|between:2008,' . date('Y')]);
+        //$request->module_id = (int)$request->module_id;
+        $request->merge(['module_id' => (int)$request->get('module_id')]);
+        $request->validate(['title' => 'required|string', 'description' => 'nullable|string', 'google_drive' => 'nullable|url|max:255', 'publish_year' => 'required|numeric|digits:4|between:2008,' . date('Y'), 'module_id' => 'required|integer|exists:modules,id']);//        exit(var_dump($request->get('module_id')));
+        ;
         MyResource::create($request->all());
         return redirect()->route('resources.index')->with('success', 'Resource Added!');
     }
@@ -98,7 +99,8 @@ class ResourceController extends Controller
     public function edit($id)
     {
         $resource = MyResource::find($id);
-        return view('resources.edit', compact('resource'));
+        $modules = Module::getFormModulesArray();
+        return view('resources.edit', compact('resource', 'modules'));
     }
 
     /**
@@ -110,7 +112,8 @@ class ResourceController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $request->validate(['title' => 'required|string', 'description' => 'nullable|string', 'google_drive' => 'nullable|url|max:255', 'publish_year' => 'required|numeric|digits:4|between:2008,' . date('Y')]);
+        $request->merge(['module_id' => intval($request->get('module_id'))]);
+        $request->validate(['title' => 'required|string', 'description' => 'nullable|string', 'google_drive' => 'nullable|url|max:255', 'publish_year' => 'required|numeric|digits:4|between:2008,' . date('Y'), 'module_id' => 'required|integer|exists:modules,id']);
         MyResource::find($id)->update($request->all());
 
         return redirect()->route('resources.index')
