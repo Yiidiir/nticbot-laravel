99a03aede021bebda5d9347a1ceadb8c8a2993f4
Yidir <yidirbouhadjer@gmail.com>
2018-02-19 00:12:11 +0100

    mend
    Added faulty Bootstrap resource bug (dirty fix for path problem) to ignore list

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -7,6 +7,7 @@ use App\Module;
 use Illuminate\Http\Request;
 use App\Resource as MyResource;
 use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Support\Facades\Auth;
 
 class ResourceController extends Controller
 {
@@ -27,7 +28,7 @@ class ResourceController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth', ['except' => ['show']]);
     }
 
 
@@ -40,7 +41,11 @@ class ResourceController extends Controller
     {
         //
 
-        $resources = MyResource::latest()->paginate(5);
+        if (Auth::user()->hasRole('Admin')) {
+            $resources = MyResource::latest()->paginate(5);
+        } else {
+            $resources = MyResource::where('user_id', Auth::user()->id)->latest()->paginate(5);
+        }
 
         return view('resources.index', compact('resources'))
             ->with('i', (request()->input('page', 1) - 1) * 5);
@@ -96,11 +101,15 @@ class ResourceController extends Controller
      * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($id, Request $request)
     {
         $resource = MyResource::find($id);
-        $modules = Module::getFormModulesArray();
-        return view('resources.edit', compact('resource', 'modules'));
+        if ($request->user()->id == Auth::user()->id || Auth::user()->hasRole('Admin')) {
+            $modules = Module::getFormModulesArray();
+            return view('resources.edit', compact('resource', 'modules'));
+        } else {
+            abort(403);
+        }
     }
 
     /**
@@ -112,12 +121,18 @@ class ResourceController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $request->merge(['module_id' => intval($request->get('module_id'))]);
-        $request->validate(['title' => 'required|string', 'description' => 'nullable|string', 'google_drive' => 'nullable|url|max:255', 'publish_year' => 'required|numeric|digits:4|between:2008,' . date('Y'), 'module_id' => 'required|integer|exists:modules,id']);
-        MyResource::find($id)->update($request->all());
+        if (MyResource::find($id)->user->id == $request->user()->id || $request->user()->hasRole('Admin')) {
 
-        return redirect()->route('resources.index')
-            ->with('success', 'Resource updated successfully');
+            $request->merge(['module_id' => intval($request->get('module_id'))]);
+            $request->validate(['title' => 'required|string', 'description' => 'nullable|string', 'google_drive' => 'nullable|url|max:255', 'publish_year' => 'required|numeric|digits:4|between:2008,' . date('Y'), 'module_id' => 'required|integer|exists:modules,id']);
+            MyResource::find($id)->update($request->all());
+
+            return redirect()->route('resources.index')
+                ->with('success', 'Resource updated successfully');
+        } else {
+            return exit('NOPE - NO ACCESS!');
+            //return abort(403);
+        }
     }
 
     /**
