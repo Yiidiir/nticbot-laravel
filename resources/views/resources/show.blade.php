9a8647ddc3ccd837dae4f8673095290af4632254
Yidir <yidirbouhadjer@gmail.com>
2018-03-09 17:05:51 +0100

    Fixed layout and added title section

--- resources/views/resources/show.blade.php
+++ resources/views/resources/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-
+@section('title', $resource->title.' ('.$resource->module->code.') ')
 @section('content')
 
     <div class="container col-md-6 col-md-offset-3">
@@ -9,63 +9,48 @@
 
             <div class="pull-left">
 
-                <h2> Show Resource</h2>
 
             </div>
 
             <div class="pull-right">
                 @if(Auth::user()!==null)
-                    <a class="btn btn-primary" href="{{ route('resources.index') }}"> Back</a>
+                    <div class="form-group">
+                        <a class="btn btn-primary" href="{{ route('resources.index') }}"> Back</a>
+                    </div>
                 @endif
             </div>
 
         </div>
 
 
-        <div class="row">
+        <div class="panel panel-default">
+            <div class="panel-heading">{{$resource->title.' ('.$resource->module->code.') '}}
+            </div>
+            <div class="panel-body">
+                <div class="form-group">
+                <span class="label label-primary">{{\App\Module::getDegree($resource->module->degree)}}</span>
+                <span class="label label-success">{{'Semester '.$resource->module->semester}}</span>
+                <span class="label label-warning">{{$resource->module->name}}</span>
+                <span class="label label-default">{{$resource->publish_year.' - '.($resource->publish_year+1)}}</span>
+                </div>
+
+                    <p class="lead">
+                        {{ $resource->description}}
+                    </p>
+
+                <div class="div-group">
 
 
-            <div class="div-group">
+                    <a type="button" href="{{$resource->google_drive}}" class="btn btn-primary btn-lg"
+                       aria-label="Left Align">
+                        <span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span> Download Resource
+                    </a>
 
-                <strong>Title:</strong>
 
-                {{ $resource->title}}
+                </div>
+
 
             </div>
-
-
-            <div class="div-group">
-
-                <strong>Body:</strong>
-
-                {{ $resource->description}}
-
-            </div>
-
-            <div class="div-group">
-
-                <strong>Year:</strong>
-
-                {{ $resource->publish_year}}
-
-            </div>
-
-
-            <div class="div-group">
-
-                <strong>Module:</strong>
-
-                {{ $resource->module->name}}
-
-            </div>
-
-
-            <div class="div-group">
-
-                <strong>Download :</strong>
-                {{ link_to($resource->google_drive, $title = 'Download Resource', $attributes = array(), $secure = null)}}
-            </div>
-
 
         </div>
 
