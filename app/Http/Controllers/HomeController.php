d0de016120c8af020972e169a30e09d03d79b99a
Yidir <yidirbouhadjer@gmail.com>
2018-03-12 22:36:19 +0100

    Added homepage logic

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Announcement;
+use App\Module;
+use App\Resource;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -32,9 +34,31 @@ class HomeController extends Controller
                 return redirect()->route('resources.index')->with('success', 'Welcome Back, Mr. ' . $request->user()->name);
             }
         } else {
-            $params = ['degree' => $request->route('degree'), 'semester' => $request->route('semester'), 'module'=>$request->route('module')];
+            $params_data = ['degree' => $request->route('degree'), 'semester' => $request->route('semester'), 'module' => $request->route('module')];
             $latest_announcement = Announcement::all()->where('planned_time', '<=', Carbon::now()->toDateTimeString())->last();
-            return view('home', compact('params','latest_announcement'));
+            if ($params_data['degree'] !== null && $params_data['semester'] === null) {
+                $params_data['semesters'] = Module::where(['degree' => $params_data['degree']])->distinct()->pluck('semester')->toArray();
+                if ($params_data['semesters'] == null) {
+                    abort('404', 'No Resources for this Semester');
+                }
+            } else {
+                if ($params_data['semester'] !== null && $params_data['module'] === null) {
+                    $params_data['modules'] = Module::where(['degree' => $params_data['degree'], 'semester' => $params_data['semester']])->has('resources')->distinct()->get();
+                    if ($params_data['modules'] == null) {
+                        abort('404', 'No Resources for this Semester');
+                    }
+                } elseif ($params_data['semester'] !== null && $params_data['module'] !== null) {
+
+                    $params_data['resources'] = Resource::whereHas('module', function ($q) use ($params_data) {
+                        $q->where('code', $params_data['module']);
+                    })->latest()->get();
+                    if ($params_data['resources'] == null) {
+                        abort('404', 'No Resources for this Semester');
+                    }
+
+                }
+            }
+            return view('home', compact('params_data', 'latest_announcement'));
         }
     }
 }
